a051ed0793a64efc7ac0c5e990c11b49a58b1f35
evgeniybazhin <hits12312@gmail.com>
2020-03-20 14:54:48 +0300

    Test Dynamic Attributes Integration

--- bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
+++ bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Mar 20, 2020, 2:40:51 PM                    ---
+ * --- Generated at Mar 20, 2020, 2:50:02 PM                    ---
  * ----------------------------------------------------------------
  */
 package concerttours.constants;
